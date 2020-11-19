0b2c38a598a73a8b1aa032d63d3610a765714eea
Evans <evansm54@GBSFYVL001M7922.local>
2020-11-19 07:52:40 +0000

    cors ALLOW ALL

--- app.js
+++ app.js
@@ -19,11 +19,19 @@ const server = http.createServer(app);
 
 // middleware
 app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://holyduck-8ecjz.ondigitalocean.app/",
-    ],
+  app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    next();
   })
 );
 
