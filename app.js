9d961149e02563f46ca5a00ac563ca9947ee7d1d
Evans <evansm54@GBSFYVL001M7922.local>
2020-11-19 08:36:29 +0000

    add token to headers

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(function (req, res, next) {
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type,Accept,Authorization"
+    "X-Requested-With,content-type,Accept,Authorization,token"
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
 
